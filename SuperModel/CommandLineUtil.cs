135b06eccef92e35424465a34963daabb229e92c
drexcska <drexcska@A75105>
2018-01-12 08:57:59 +0100

    Add project files.

---
+++ SuperModel/CommandLineUtil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperModel
+{
+    public class CommandLineUtil
+    {
+        public static bool HasFlag(string[] args, string id)
+        {
+            var parms = args.Where(a => (a.StartsWith("-") || a.StartsWith("/")));
+            foreach (var p in parms)
+            {
+                var flag = p.ToUpper();
+                if (flag.Length == 2 && flag[1].ToString() == id.ToUpper())
+                    return true;
+            }
+
+            return false;
+        }
+        public static bool ParseArg(string[] args, string id, out string res)
+        {
+            res = string.Empty;
+
+            var parms = args.Where(a => (a.StartsWith("-") || a.StartsWith("/")) && a.Contains(":"));
+            foreach (var p in parms)
+            {
+                int colon = p.IndexOf(":");
+                string argName = p.Substring(1, colon - 1).ToLower();
+                if (id.ToLower() == argName.ToLower())
+                {
+                    colon++;
+                    res = p.Substring(colon, p.Length - colon);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool ParseArg(string[] args, string id, out string[] res)
+        {
+            res = null;
+            string list;
+            if (!ParseArg(args, id, out list))
+                return false;
+            res = list.Split(',', ';');
+            return true;
+        }
+    }
+}
---
+++ SuperModel/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperModel
+{
+
+    public static class Extensions
+    {
+        public static string[] ToLower(this string[] str)
+        {
+            var str2 = new List<string>();
+            foreach (var s in str)
+            {
+                str2.Add(s.ToLower());
+            }
+            return str2.ToArray();
+        }
+
+
+    }
+}
